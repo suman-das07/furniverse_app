82b0c7a1e215f2ed56d2403112850e08c5b7cdd0
SUMAN DAS <sumanda0708@gmail.com>
2025-12-30 17:49:44 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/furniverse_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.furniverse_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(FurniverseApp());
+}
+
+class FurniverseApp extends StatelessWidget {
+  const FurniverseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: ,
+    );
+  }
+}
